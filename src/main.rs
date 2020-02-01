8d201d656a240e4fe8e6761b4d47dd5bb3a8fed1
Lucio Franco <luciofranco14@gmail.com>
2020-02-01 16:03:24 -0500

    better bench
    Signed-off-by: Lucio Franco <luciofranco14@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,12 @@ pub mod pb {
 const ADDR: &'static str = "127.0.0.1:8080";
 
 use metrics_runtime::{exporters::LogExporter, observers::YamlBuilder, Receiver};
-use std::time::{Duration, Instant};
+use quanta::Clock;
+use std::time::Duration;
 
 fn main() -> anyhow::Result<()> {
-    pretty_env_logger::init_custom_env("info");
+    std::env::set_var("RUST_LOG", "info");
+    pretty_env_logger::init();
 
     let recv = Receiver::builder()
         .build()
@@ -44,13 +46,13 @@ fn main() -> anyhow::Result<()> {
     let addr = ADDR.parse()?;
     rt1.spawn(server::run(addr));
 
-    println!("Sending requests...");
+    log::info!("Sending requests...");
 
     rt2.block_on(async move {
         run_client().await.unwrap();
     });
 
-    println!("done sending requests");
+    log::info!("done sending requests");
 
     exporter.turn();
 
@@ -58,22 +60,38 @@ fn main() -> anyhow::Result<()> {
 }
 
 async fn run_client() -> anyhow::Result<()> {
-    let conn = tonic::transport::Channel::from_shared(format!("http://{}", ADDR))?
-        .connect()
-        .await?;
+    let clock = Clock::new();
 
-    let mut client = pb::echo_client::EchoClient::new(conn);
+    let mut j = Vec::new();
+    for _ in 0..400 {
+        let clock = clock.clone();
 
-    for _ in 0..10_000 {
-        let request = tonic::Request::new(pb::EchoRequest {
-            message: "hello".into(),
-        });
+        j.push(tokio::spawn(async move {
+            let conn = tonic::transport::Channel::from_shared(format!("http://{}", ADDR))
+                .unwrap()
+                .connect()
+                .await
+                .unwrap();
 
-        let start = Instant::now();
-        client.unary_echo(request).await?;
-        let end = Instant::now();
+            let mut client = pb::echo_client::EchoClient::new(conn);
 
-        metrics::timing!("request_duration", start, end);
+            for _ in 0..100 {
+                let request = tonic::Request::new(pb::EchoRequest {
+                    message: "hello".into(),
+                });
+
+                let start = clock.start();
+                client.unary_echo(request).await.unwrap();
+                let end = clock.end();
+
+                let time_ms = clock.delta(start, end) / 1_000_000;
+                metrics::timing!("request_duration_ms", time_ms);
+            }
+        }));
+    }
+
+    for j in j {
+        j.await.unwrap();
     }
 
     Ok(())
